a94d4e8ad9e86f0bd59ee242de28c3926be272a1
Mahlet Riddic <114420037+MahletRiddick@users.noreply.github.com>
2023-11-27 13:24:07 +0300

    update 1:23
    update

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// AOS.init();
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // Set the offset (in pixels) from the original trigger point for when animations should start
+  delay: 0, // Specify the animation delay in milliseconds (0 means no delay)
+  duration: 700, // Set the duration of the animations in milliseconds
+  easing: 'ease', // Define the easing function for AOS animations (e.g., 'ease', 'linear', 'ease-in-out')
+  once: false, // If set to true, the animation will occur every time an element enters the viewport while scrolling
+  mirror: false, // If set to true, elements will not animate out when scrolling back up
+  anchorPlacement: 'top-bottom', // Define the position of the element relative to the window that triggers the animation
+
+});
